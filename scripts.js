33c27f838075faf53cb637dcc7f083462e259c53
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2018-01-16 12:23:44 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,4 @@
 
-function popBalloon() {
-  document.getElementsByTagName("img")[Math.floor(Math.random() * 5)].style.display = "block";
-}
 document.getElementById("button").addEventListener("click", popBalloon);
 
       var start = new Date().getTime();
